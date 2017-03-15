eadd99d6e9802a565047deb51cc8e51549957bf7
manish-mittal12 <manish_mittal12@yahoo.co.in>
2017-03-15 17:39:44 +0530

    new file

--- Dockerfile
+++ Dockerfile
@@ -2,54 +2,29 @@ From manishmittal0788/wordpress
 LABEL Maintainer="Manish Mittal" \
       Description="WordPress container with version 3 compose.yml for deploying over swarm"
 
-
-ADD * /WordpressCompose
+# Expose environment variables
+ENV DB_HOST wp-mysql
+ENV DB_PORT 3306
+ENV DB_NAME wordpress
+ENV DB_USER root
+ENV DB_PASS wordpress
+ENV ROOT_PATH /
 
 # wp-content volume
 VOLUME /var/www/html
 WORKDIR /var/www/html
 RUN chown -R root.root /var/www/html
 
-
 RUN mkdir -p /usr/src
 ADD * /usr/src/
 RUN chown -R nobody.nobody /usr/src/wordpress
 
-# WP config
-COPY wp-config.php /usr/src/wordpress
-RUN chown nobody.nobody /usr/src/wordpress/wp-config.php && chmod 640 /usr/src/wordpress/wp-config.php
-
-
-# Entrypoint to copy wp-content
-COPY entrypoint.sh /entrypoint.sh
-ENTRYPOINT [ "/entrypoint.sh" ]
-
-
-COPY docker-entrypoint.sh /usr/local/bin/
-RUN chmod +x /usr/local/bin/docker-entrypoint.sh
-ENTRYPOINT ["docker-entrypoint.sh"]
-
-EXPOSE 8001 80 3306 443
-
-CMD ["apache2-foreground"]
-
-# Modify permissions to allow plugin upload
-RUN chown -R www-data:www-data /app/wp-content /var/www/html
-
-
-# Expose environment variables
-ENV DB_HOST **LinkMe**
-ENV DB_PORT **LinkMe**
-ENV DB_NAME wordpress
-ENV DB_USER admin
-ENV DB_PASS **ChangeMe**
-ENV ROOT_PATH /
-
 # Configure Wordpress to connect to local DB
 ADD wp-config.php /app/wp-config.php
 
-# Configure Wordpress to connect to local DB
-COPY wp-config.php /app/wp-config.php
+# WP config
+COPY wp-config.php /usr/src/wordpress
+RUN chown nobody.nobody /usr/src/wordpress/wp-config.php && chmod 640 /usr/src/wordpress/wp-config.php
 
 # Add scripts and make them executable.
 COPY create_mysql_admin_user.sh /create_mysql_admin_user.sh
@@ -60,5 +35,12 @@ RUN chmod +x /*.sh
 # Add volumes for MySQL and the application.
 VOLUME ["/app/wp-content"]
 VOLUME ["/var/lib/mysql", "/var/www/html"]
+
+# Entrypoint to copy wp-content
+COPY entrypoint.sh /usr/local/bin/
+RUN chmod +x /usr/local/bin/entrypoint.sh
+ENTRYPOINT ["entrypoint.sh"]
+
+EXPOSE 8001 80 3306 443
 
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,76 +1,31 @@
 #!/bin/bash
-if [ "$ROOT_PATH" != "/" ]
-then
-    unlink /var/www/html
-    mkdir /var/www/html
-    ln -s /app /var/www/html/$ROOT_PATH
-    sed -i "s/ROOT_PATH/\/$ROOT_PATH/g" /app/.htaccess
+
+VOLUME_HOME="/var/lib/mysql"
+APPLICATION_HOME="/var/www/html"
+
+sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
+    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
+
+# Check if VOLUME_HOME has content
+if [[ ! -d $VOLUME_HOME/mysql ]]; then
+    echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
+    echo "=> Installing MySQL ..."
+    mysql_install_db > /dev/null 2>&1
+    echo "=> Done!"  
+    /create_mysql_admin_user.sh
 else
-    sed -i "s/ROOT_PATH//g" /app/.htaccess
+    echo "=> Using an existing volume of MySQL"
 fi
 
-chown www-data:www-data /app -R
-chmod -R 777 /app/wp-content
+# If the application directory is empty, copy the site.
+if [ ! "$(ls -A $APPLICATION_HOME)" ]; then
+    echo "=> Copying the site"
 
-DB_HOST=${DB_PORT_3306_TCP_ADDR:-${DB_HOST}}
-DB_HOST=${DB_1_PORT_3306_TCP_ADDR:-${DB_HOST}}
-DB_PORT=${DB_PORT_3306_TCP_PORT:-${DB_PORT}}
-DB_PORT=${DB_1_PORT_3306_TCP_PORT:-${DB_PORT}}
+    # Copy the application folder.
+    cp -r /app/. $APPLICATION_HOME
 
-if [ "$DB_PASS" = "**ChangeMe**" ] && [ -n "$DB_1_ENV_MYSQL_PASS" ]; then
-    DB_PASS="$DB_1_ENV_MYSQL_PASS"
+    # Configure ownership to allow plugin upload.
+    chown -R www-data:www-data $APPLICATION_HOME/wp-content
 fi
 
-echo "=> Using the following MySQL/MariaDB configuration:"
-echo "========================================================================"
-echo "      Database Host Address:  $DB_HOST"
-echo "      Database Port number:   $DB_PORT"
-echo "      Database Name:          $DB_NAME"
-echo "      Database Username:      $DB_USER"
-echo "========================================================================"
-
-if [ -f /.mysql_db_created ]; then
-        source /etc/apache2/envvars
-        exec apache2 -D FOREGROUND
-fi
-
-for ((i=0;i<10;i++))
-do
-    DB_CONNECTABLE=$(mysql -u$DB_USER -p$DB_PASS -h$DB_HOST -P$DB_PORT -e 'status' >/dev/null 2>&1; echo "$?")
-    if [[ DB_CONNECTABLE -eq 0 ]]; then
-        break
-    fi
-    sleep 5
-done
-
-if [[ $DB_CONNECTABLE -eq 0 ]]; then
-    DB_EXISTS=$(mysql -u$DB_USER -p$DB_PASS -h$DB_HOST -P$DB_PORT -e "SHOW DATABASES LIKE '"$DB_NAME"';" 2>&1 |grep "$DB_NAME" > /dev/null ; echo "$?")
-
-    if [[ DB_EXISTS -eq 1 ]]; then
-        echo "=> Creating database $DB_NAME"
-        RET=$(mysql -u$DB_USER -p$DB_PASS -h$DB_HOST -P$DB_PORT -e "CREATE DATABASE $DB_NAME")
-        if [[ RET -ne 0 ]]; then
-            echo "Cannot create database for wordpress"
-            exit RET
-        fi
-        if [ -f /initial_db.sql ]; then
-            echo "=> Loading initial database data to $DB_NAME"
-            RET=$(mysql -u$DB_USER -p$DB_PASS -h$DB_HOST -P$DB_PORT $DB_NAME < /initial_db.sql)
-            if [[ RET -ne 0 ]]; then
-                echo "Cannot load initial database data for wordpress"
-                exit RET
-            fi
-        fi
-        echo "=> Done!"    
-    else
-        echo "=> Skipped creation of database $DB_NAME – it already exists."
-    fi
-else
-    echo "Cannot connect to Mysql"
-    exit $DB_CONNECTABLE
-fi
-
-touch /.mysql_db_created
-
-source /etc/apache2/envvars
-exec apache2 -D FOREGROUND
+exec supervisord -n
